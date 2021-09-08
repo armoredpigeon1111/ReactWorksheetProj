2a55146623b52c03349c51544b0c211051b538ef
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-08 12:21:07 -0400

    Added DisplayName.jsx component and seed data to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
 import React, {Component} from "react";
+import './App.css'
+import DisplayName from './DisplayName/DisplayName'
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            
+            firstName: 'Reggie',
+            lastName: 'White',
+            names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kelly'],
+            superheroes : [
+                {
+                    superheroId: 1,
+                    name: 'Batman',
+                    primaryAbility: 'Wealthy',
+                    secondarAbility: 'Rich'
+                },
+                {
+                    superheroId: 2,
+                    name: 'Superman',
+                    primaryAbility: 'Super strength',
+                    secondarAbility: 'Fly'
+                },
+                {
+                    superheroId: 3,
+                    name: 'Spiderman',
+                    primaryAbility: 'Spider senses',
+                    secondarAbility: 'Shoots web'
+                }
+            ]
         };
     }
 
     render(){
         return(
+            <div className="container-fluid">
             <h1>React Worksheet</h1>
+            <DisplayName firstName = {this.state.firstName} lastName = {this.state.lastName} />
+            </div>
+
         )
     }    
 }
