b51499c9415eb687f67cfca90a537b89bfc0e72e
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-09 14:44:11 -0400

    Fixed jokes to save jokes in jokesFromAPI and display them

--- src/components/Jokes/Jokes.jsx
+++ src/components/Jokes/Jokes.jsx
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
 
 class Jokes extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            jokesFromAPI: []
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokesFromAPI: [ ],
+    };
+  }
 
+  componentDidMount() {
+    this.addJoke();
+  }
 
-    componentDidMount(){
-        this.addJoke();
-    }
+  async addJoke() {
+    let response = await axios.get(
+      "https://v2.jokeapi.dev/joke/Programming?type=twopart&amount=5"
+    );
+    console.log(response.data.setup);
+    console.log(response.data.delivery);
+    console.log(response);
+    this.setState({
+      jokesFromAPI: response.data.jokes,
+    });
+  }
 
-    async addJoke(){
-        
-        let response = await axios.get('https://v2.jokeapi.dev/joke/Programming?type=twopart&amount=5');
-        console.log(response.data.setup);
-        console.log(response.data.delivery);
-        this.setState({
-            jokesFromAPI: response.data
-        })
-    }
-
-
-    render() { 
-        return ( 
-            this.state.jokesFromAPI.map(joke =>{
-                <span>
-                    <h1>{joke.setup}</h1>
-                    <h1>{joke.delivery}</h1>    
-                </span>
-            })
-        
-         );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.jokesFromAPI.map((joke) => {
+          return (
+            <span>
+              <h1>{joke.setup}</h1>
+              <h1>{joke.delivery}</h1>
+            </span>
+          );
+        })}
+      </div>
+    );
+  }
 }
- 
+
 export default Jokes;
